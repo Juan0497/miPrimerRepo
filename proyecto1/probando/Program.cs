a8de8377694907c224c7fa9818b1ce4d32acf41d
Juan Fernandez <juan97f@gmail.com>
2025-10-02 21:36:59 -0600

    ultimas actualizaciones

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api key = "sk_test_51H8x";
+string stripe api key = "sk_test_51H8x";
